b6831ff94b56d680609acce11747f950c46d3cde
rdubbs <roger_dubbs@yahoo.com>
2017-06-21 13:50:33 -0700

    Added User class and deleted main.

---
+++ test/application/UserAccountCreationTest.java
@@ -0,0 +1,17 @@
+package application;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserAccountCreationTest {
+    String firstName;
+    String lastName;
+    String userEmail;
+    String password;
+    @Test
+    public void testCreateUser()
+    {
+        User testsubject = new User ();
+    }
+}

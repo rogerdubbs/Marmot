87c8488bc9f3c6c8e09febb1cf7c9fc1b2f275ab
Roger Dubbs <rdubbs@mac-rdubbs.tsi.lan>
2017-06-21 12:08:54 -0700

    Template version.

---
+++ test/SomeTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SomeTest {
+    @Test
+    public void testSomething() {
+        assertEquals(0, 0);
+    }
+}

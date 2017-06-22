931a07916a3e3026807b2eea2adbf530482f5047
rdubbs <roger_dubbs@yahoo.com>
2017-06-22 14:10:09 -0700

    Populated auction class.

--- src/application/Auction.java
+++ src/application/Auction.java
@@ -1,4 +1,24 @@
 package application;
 
+import java.util.Date;
+
 class Auction {
+    private User seller;
+    private final String itemDescription;
+    private final double startingPrice;
+    private final Date startTime;
+    private final Date endTime;
+
+    Auction(User seller, String itemDescription, double startingPrice, Date startTime, Date endTime) {
+
+        this.seller = seller;
+        this.itemDescription = itemDescription;
+        this.startingPrice = startingPrice;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    User getSeller() {
+        return seller;
+    }
 }
--- test/application/CreateAuctionTest.java
+++ test/application/CreateAuctionTest.java
@@ -3,6 +3,9 @@ package application;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.Instant;
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 
 public class CreateAuctionTest {
@@ -21,6 +24,18 @@ public class CreateAuctionTest {
 
     @Test
     public void canCreateAuction() {
-        Auction auction = new Auction();
+        // User must be logged in an a seller.
+        // Start Time > Now.
+        // End Time > Start Time
+        // Seller cannot bid on their own auction
+        User user = users.findByUserName(UsersTestHelper.USER_NAME);
+        users.login(UsersTestHelper.USER_NAME,UsersTestHelper.USER_PASSWORD);
+        String itemDescription = "Best non-car non-software item ever";
+        long currentTimeMillis = System.currentTimeMillis();
+        Date startTime = new Date(currentTimeMillis + 1000000);
+        Date endTime = new Date(currentTimeMillis + 2000000);
+        double startingPrice = 0.99;
+        Auction auction = new Auction(user, itemDescription, startingPrice, startTime, endTime);
+        assertEquals(user,auction.getSeller());
     }
 }
